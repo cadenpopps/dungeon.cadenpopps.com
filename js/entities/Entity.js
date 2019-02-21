329a8c5c53b53ab697d932ad86f07e0ba4bd831b
Caden Popps <cadenpopps@gmail.com>
2019-02-21 14:17:08 -0600

    working

--- js/entities/Entity.js
+++ js/entities/Entity.js
@@ -5,10 +5,12 @@ const IDLE = 4;
 const NUMBER_MOVES = 4;
 const FAIL = 0, SUCCESS = 1;
 
-function Entity(pos, hp = 1, str = 1, mag = 1, int = 1, speed = MEDIUM, animations = undefined) {
+function Entity(pos, hp = 3, str = 1, mag = 1, int = 1, speed = MEDIUM, animations = undefined) {
     this.x = pos[0];
     this.y = pos[1];
     this.health = hp;
+	this.currentHealth = hp;
+	this.currentHealth -= 1;
     this.strength = str;
     this.magic = mag;
     this.intelligence = int;
@@ -70,43 +72,47 @@ Entity.prototype.move = function (dir, board, mobs) {
 
 Entity.prototype.updateMobs = function (mobs, dir) {
     switch (dir) {
-        case UP:
-            delete mobs[getSquareCode(this.x, this.y + 1)];
-            break;
-        case RIGHT:
-            delete mobs[getSquareCode(this.x - 1, this.y)];
-            break;
-        case DOWN:
-            delete mobs[getSquareCode(this.x, this.y - 1)];
-            break;
-        case LEFT:
-            delete mobs[getSquareCode(this.x + 1, this.y)];
-            break;
-        default:
-            console.log("No direction");
-            break;
-    }
-    mobs[getSquareCode(this.x, this.y)] = this;
+		case UP:
+			delete mobs[getSquareCode(this.x, this.y + 1)];
+			break;
+		case RIGHT:
+			delete mobs[getSquareCode(this.x - 1, this.y)];
+			break;
+		case DOWN:
+			delete mobs[getSquareCode(this.x, this.y - 1)];
+			break;
+		case LEFT:
+			delete mobs[getSquareCode(this.x + 1, this.y)];
+			break;
+		default:
+			console.log("No direction");
+			break;
+	}
+	mobs[getSquareCode(this.x, this.y)] = this;
+}
+
+Entity.prototype.healthPercent = function(){
+	return floor(this.currentHealth/this.health * 100) / 100;
 }
 
 Entity.prototype.attack = function () {
-    console.log("To implement");
+	console.log("To implement");
 }
 
 Entity.prototype.animate = function (idleTimer) {
-    if (this.animation == IDLE) {
-        if (idleTimer == 0) {
-            this.animationCounter++;
-        }
-    }
-    else {
-        this.animationCounter++;
-    }
-    if (this.animationCounter < this.animations[this.animation].length) {
-        this.sprite = this.animations[this.animation][this.animationCounter];
-    }
-    else {
-        this.animationCounter = 0;
+	if (this.animation == IDLE) {
+		if (idleTimer == 0) {
+			this.animationCounter++;
+		}
+	}
+	else {
+		this.animationCounter++;
+	}
+	if (this.animationCounter < this.animations[this.animation].length) {
+		this.sprite = this.animations[this.animation][this.animationCounter];
+	}
+	else {
+		this.animationCounter = 0;
         this.animation = IDLE;
         this.busy = false;
     }
