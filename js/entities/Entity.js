c621a3fe7a5823c9cc4c9b11804c90978bd52d24
Caden Popps <cadenpopps@gmail.com>
2019-03-07 13:45:22 -0600

    fixed attacking dead

--- js/entities/Entity.js
+++ js/entities/Entity.js
@@ -82,12 +82,12 @@ Entity.prototype.move = function (dir, board, mobs) {
             console.log("No direction");
             break;
     }
-    if (status != FAIL) {
-        this.animation = dir;
-        this.animationCounter = 0;
-        this.busy = true;
-    }
-    return status;
+	if (status != FAIL) {
+		this.animation = dir;
+		this.animationCounter = 0;
+		this.busy = true;
+	}
+	return status;
 }
 
 Entity.prototype.healthPercent = function(){
@@ -100,7 +100,9 @@ Entity.prototype.takeDamage = function(rawDamage = 0){
 }
 
 Entity.prototype.attack = function (target) {
-	target.takeDamage(this.strength);	
+	if(target.alive){
+		target.takeDamage(this.strength);	
+	}
 }
 
 Entity.prototype.animate = function (idleTimer) {
@@ -117,8 +119,8 @@ Entity.prototype.animate = function (idleTimer) {
 	}
 	else{
 		this.animationCounter = 0;
-        this.animation = IDLE;
-        this.busy = false;
+		this.animation = IDLE;
+		this.busy = false;
 	}
 }
 
